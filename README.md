c80b4a89059d2d554c0d0a5fb4f5dbf9857950c5
Matheus Santos do Nascimento <msbusiness.513@gmail.com>
2022-12-05 00:01:54 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Clone Spotify
+
+![preview](./.github/preview.png)
+
+> Projeto de Estudo
+
+Projeto construído para fortalecer estudos de JavaScript e fazer uso de funções horas.
+
+[Clique aqui para acessar](https://matheus-santos01.github.io/clone-spotify/)
+
+## 🛠️ Tecnologias
+- HTML 
+- CSS
+- Git e Github
+
+## Contato
+
+msbusiness.513@gmail.com
---
+++ js/main.js
@@ -0,0 +1,39 @@
+function createBoxHours(hour){
+    if(hour < 10) {hour = "0" + hour}
+    return `
+        <div class="box">
+            <span class="hour">${hour}</span>
+            <span></p>
+        </div>
+    `
+}
+
+function createBoxMinutes(minutes){
+    if(minutes < 10) {minutes = "0" + minutes}
+    return `
+        <div class="box">
+            <span class="minutes">${minutes}</span>
+            <span>minutos</p>
+        </div>
+    `
+}
+
+function createBoxSeconds(seconds){
+
+    if(seconds < 10) {seconds = "0" + seconds}
+    return `
+        <div class="box">
+            <span class="seconds">${seconds}</span>
+            <span>segundos</p>
+        </div>
+    `
+}
+
+
+setInterval(function time(){
+
+    document.querySelector(".modal").innerHTML =
+    createBoxHours(new Date().getHours()) +
+    createBoxMinutes(new Date().getMinutes()) + 
+    createBoxSeconds(new Date().getSeconds())
+})
